12ba3e7d5205070b14826abe080e00c382fe710a
manvinderjit <manvinderjit@gmail.com>
2022-08-21 20:46:02 -0400

    Initial Commit

--- scripts.js
+++ scripts.js
@@ -1,12 +1,23 @@
-let myLibrary = [];
-
-function Book(title, author, pages, read) {    
+class Library { 
+  
+  constructor(title, author, pages, read) {
+    let myLibrary = [];
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
-    
+  }
 }
+
+
+
+// function Book(title, author, pages, read) {    
+//     this.title = title;
+//     this.author = author;
+//     this.pages = pages;
+//     this.read = read;
+    
+// }
 
 function addBookToLibrary(newBook) {  
   myLibrary.push(newBook);
